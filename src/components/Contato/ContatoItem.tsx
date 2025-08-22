93ad269452fe0f04a928a158e10154b9c5a55075
Mateus Maranhão <mateusnmaranhao@gmail.com>
2025-08-22 16:15:53 -0300

    update contato

--- src/components/Contato/ContatoItem.tsx
+++ src/components/Contato/ContatoItem.tsx
@@ -14,7 +14,7 @@ export default function ContatoItem({ href, icon, texto }: ContatoItemProps) {
             className='flex items-center gap-4 bg-purple-800/10 border border-purple-800/20 p-4 rounded-xl hover:shadow-[0_0_15px_#ffffff22] hover:bg-purple-400/20 hover:scale-[1.02] transition'
         >
             <div className="shrink-0">{icon}</div>
-            <span className='text-white text-sm hidden md:block'>{texto}</span>
+            <span className='text-white text-sm'>{texto}</span>
         </a>
     )
 }
