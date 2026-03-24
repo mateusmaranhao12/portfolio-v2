a035a75304691c36c6a06a9f7f294f48f28d0be4
mateusmaranhao12 <mateusnm2003@gmail.com>
2026-03-24 11:00:45 -0300

    update footer light mode

--- src/app/modules/Footer.tsx
+++ src/app/modules/Footer.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useTheme } from "@/components/theme/theme";
 export default function Footer() {
+    const { isDark } = useTheme();
     return (
-        <footer className="bg-purple-950 w-full py-6 text-center text-sm text-slate-400">
+        <footer className={`w-full py-6 text-center font-bold text-sm ${isDark ? "bg-purple-950 text-slate-400" : "bg-slate-100 text-slate-800"}`}>
             &copy; {new Date().getFullYear()} Mateus Maranhao. Todos os direitos reservados.
         </footer>
     );
