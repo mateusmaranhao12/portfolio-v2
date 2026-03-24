4ca47bfef060582b1f32f7046c3ae696fea58e87
mateusmaranhao12 <mateusnm2003@gmail.com>
2026-03-24 10:58:38 -0300

    update contato light mode

--- src/app/modules/Contato.tsx
+++ src/app/modules/Contato.tsx
@@ -1,61 +1,80 @@
-'use client'
-import SectionWrapper from '@/components/Common/SectionWrapper';
-import SectionTitle from '@/components/Common/SectionTitle';
-import { FaWhatsapp, FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
-import ContatoItem from '@/components/Contato/ContatoItem';
-import { motion } from 'framer-motion';
-
-const contatos = [
-    {
-        nome: 'WhatsApp',
-        href: 'https://wa.me/5579991762846',
-        icon: <FaWhatsapp size={28} className='text-green-400' />,
-        texto: 'Entre em contato diretamente comigo via WhatsApp',
-    },
-    {
-        nome: 'E-mail',
-        href: 'mailto:mateusnm2003@gmail.com',
-        icon: <FaEnvelope size={28} className='text-red-400' />,
-        texto: 'mateusnm2003@gmail.com',
-    },
-    {
-        nome: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/mateusnmaranhao/',
-        icon: <FaLinkedin size={28} className='text-blue-500' />,
-        texto: 'Conecte-se comigo no LinkedIn agora mesmo!',
-    },
-    {
-        nome: 'GitHub',
-        href: 'https://github.com/mateusmaranhao12',
-        icon: <FaGithub size={28} className='text-white' />,
-        texto: 'Venha conferir meus projetos e contribuições no GitHub!',
-    },
-];
+"use client";
+import SectionWrapper from "@/components/Common/SectionWrapper";
+import SectionTitle from "@/components/Common/SectionTitle";
+import { FaWhatsapp, FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import ContatoItem from "@/components/Contato/ContatoItem";
+import { motion } from "framer-motion";
+import { useTheme } from "@/components/theme/theme";
 
 export default function Contato() {
-    return (
-        <SectionWrapper id='contato'>
-            <motion.div
-                className='w-full'
-                initial={{ opacity: 0, y: 40 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                viewport={{ once: true, amount: 0.3 }}
-                transition={{ type: "spring", stiffness: 300, damping: 18 }}
-            >
-                <SectionTitle>Contato</SectionTitle>
-                <p className='text-slate-400 text-start mt-2'>Fique à vontade para entrar em contato comigo através dos links abaixo!</p>
-                <div className='grid grid-cols-1 sm:grid-cols-2 gap-6 mt-10 mb-20'>
-                    {contatos.map((contato, index) => (
-                        <ContatoItem
-                            key={index}
-                            nome={contato.nome}
-                            href={contato.href}
-                            icon={contato.icon}
-                            texto={contato.texto}
-                        />
-                    ))}
-                </div>
-            </motion.div>
-        </SectionWrapper>
-    );
+  const { isDark } = useTheme();
+
+  const contatos = [
+    {
+      nome: "WhatsApp",
+      href: "https://wa.me/5579991762846",
+      icon: (
+        <FaWhatsapp
+          size={28}
+          className={isDark ? "text-green-400" : "text-green-600"}
+        />
+      ),
+      texto: "Entre em contato diretamente comigo via WhatsApp",
+    },
+    {
+      nome: "E-mail",
+      href: "mailto:mateusnm2003@gmail.com",
+      icon: (
+        <FaEnvelope size={28} className={isDark ? "text-red-400" : "text-red-600"} />
+      ),
+      texto: "mateusnm2003@gmail.com",
+    },
+    {
+      nome: "LinkedIn",
+      href: "https://www.linkedin.com/in/mateusnmaranhao/",
+      icon: (
+        <FaLinkedin size={28} className={isDark ? "text-blue-400" : "text-blue-600"} />
+      ),
+      texto: "Conecte-se comigo no LinkedIn agora mesmo!",
+    },
+    {
+      nome: "GitHub",
+      href: "https://github.com/mateusmaranhao12",
+      icon: (
+        <FaGithub size={28} className={isDark ? "text-white" : "text-black"} />
+      ),
+      texto: "Venha conferir meus projetos e contribuições no GitHub!",
+    },
+  ];
+  return (
+    <SectionWrapper id="contato">
+      <motion.div
+        className="w-full"
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ type: "spring", stiffness: 300, damping: 18 }}
+      >
+        <SectionTitle>Contato</SectionTitle>
+        <p
+          className={`text-start mt-2 ${isDark ? "text-slate-300" : "text-slate-800"}`}
+        >
+          Fique à vontade para entrar em contato comigo através dos links
+          abaixo!
+        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-10 mb-20">
+          {contatos.map((contato, index) => (
+            <ContatoItem
+              key={index}
+              nome={contato.nome}
+              href={contato.href}
+              icon={contato.icon}
+              texto={contato.texto}
+              isDark={isDark ?? false}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </SectionWrapper>
+  );
 }
--- src/components/Contato/ContatoItem.tsx
+++ src/components/Contato/ContatoItem.tsx
@@ -3,6 +3,7 @@ type ContatoItemProps = {
   href: string;
   icon: React.ReactNode;
   texto?: string;
+  isDark?: boolean;
 };
 
 export default function ContatoItem({
@@ -10,19 +11,26 @@ export default function ContatoItem({
   icon,
   texto,
   nome,
+  isDark = false,
 }: ContatoItemProps) {
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex flex-col items-start gap-1 bg-purple-800/10 border-l-3 border-l-yellow-500 border border-purple-900/20 p-4 rounded-xl hover:shadow-[0_0_15px_#ffffff22] hover:bg-purple-400/20 hover:scale-[1.02] transition"
+      className={`
+        flex flex-col items-start gap-1 border-l-3 border p-4 rounded-xl hover:scale-[1.02] transition 
+        ${
+          isDark
+            ? "border-l-yellow-500 border-purple-900/20 hover:shadow-[0_0_15px_#ffffff22] hover:bg-purple-400/20"
+            : "border-l-purple-500 border-slate-500/20 hover:shadow-[0_0_15px_#00000022] hover:bg-slate-200/50"
+        }`}
     >
       <div className="flex gap-2 mb-2">
         <div className="shrink-0">{icon}</div>
-        <h3 className="text-lg font-semibold text-white">{nome}</h3>
+        <h3 className={`text-lg font-semibold ${isDark ? "text-white" : "text-black"}`}>{nome}</h3>
       </div>
-      {texto && <span className="text-slate-400 text-sm">{texto}</span>}
+      {texto && <span className={`text-sm ${isDark ? "text-slate-300" : "text-slate-800"}`}>{texto}</span>}
     </a>
   );
 }
